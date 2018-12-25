ea3f85bfecaf605505058c61b1afbb942f232ef2
Heitor Silva <heitor-silva@hotmail.com>
2018-12-26 00:17:29 +0100

    Incluir favicons

--- lib/livrodealemao_web/endpoint.ex
+++ lib/livrodealemao_web/endpoint.ex
@@ -12,8 +12,8 @@ defmodule LivrodealemaoWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :livrodealemao,
-    gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    gzip: true,
+    only: ~w(css fonts images js android-icon-36x36.png android-icon-48x48.png android-icon-72x72.png android-icon-96x96.png android-icon-144x144.png android-icon-192x192.png apple-icon-57x57.png apple-icon-60x60.png apple-icon-72x72.png apple-icon-76x76.png apple-icon-114x114.png apple-icon-120x120.png apple-icon-144x144.png apple-icon-152x152.png apple-icon-180x180.png apple-icon-precomposed.png apple-icon.png browserconfig.xml favicon-32x32.png favicon-96x96.png favicon-16x16.png favicon.ico manifest.json ms-icon-70x70.png ms-icon-144x144.png ms-icon-150x150.png ms-icon-310x310.png robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
