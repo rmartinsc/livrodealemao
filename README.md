a1b6c40b460eabaf854e81fa2a4c8bd9fdfa9cbf
Heitor Silva <heitor-silva@hotmail.com>
2018-12-25 21:09:32 +0100

    Incluir Codecov

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Livro de Alemão [![Build Status](https://travis-ci.com/livrodealemao/livrodealemao.svg?branch=master)](https://travis-ci.com/livrodealemao/livrodealemao)
+# Livro de Alemão [![Build Status](https://travis-ci.com/livrodealemao/livrodealemao.svg?branch=master)](https://travis-ci.com/livrodealemao/livrodealemao) [![codecov](https://codecov.io/gh/livrodealemao/livrodealemao/branch/master/graph/badge.svg)](https://codecov.io/gh/livrodealemao/livrodealemao)
 
 Aprenda alemão de forma fácil.
--- mix.exs
+++ mix.exs
@@ -10,7 +10,14 @@ defmodule Livrodealemao.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test
+      ],
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
@@ -42,7 +49,8 @@ defmodule Livrodealemao.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
