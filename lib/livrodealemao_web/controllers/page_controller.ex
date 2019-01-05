a7eb2d162b86de1d392d2328f917aeccc1f3431b
RodolphoCavalcanti <rodolphom@gmail.com>
2019-01-05 11:27:05 +0100

    Inclui o controlador para a view de contato

--- lib/livrodealemao_web/controllers/page_controller.ex
+++ lib/livrodealemao_web/controllers/page_controller.ex
@@ -4,4 +4,9 @@ defmodule LivrodealemaoWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def contato(conn, _params) do
+  	render(conn, "contato.html")
+  end
+  
 end
