9d0dc11bc3e32e8a3eeac1989c44fbcc47670c18
RodolphoCavalcanti <rodolphom@gmail.com>
2019-01-05 11:23:31 +0100

    Inclui HTML para o formulario e a nova rota /contato

--- lib/livrodealemao_web/router.ex
+++ lib/livrodealemao_web/router.ex
@@ -17,6 +17,8 @@ defmodule LivrodealemaoWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+
+    get "/contato", PageController, :contato
   end
 
   # Other scopes may use custom stacks.
